0015805f7dcaa96b01f135d7774741578cd40e78
SkynixFr <romain.ludovic.day@gmail.com>
2024-03-17 17:19:07 +0100

    feat(team): get one team, get all team

--- src/main/java/org/example/pokemonmasterapi/controllers/TeamController.java
+++ src/main/java/org/example/pokemonmasterapi/controllers/TeamController.java
@@ -26,4 +26,18 @@ public class TeamController {
         teamRepository.save(team);
         return ResponseEntity.status(HttpStatus.CREATED).body("Team created");
     }
+
+    @GetMapping
+    public ResponseEntity<Object> getTeams() {
+        return ResponseEntity.status(HttpStatus.OK).body(teamRepository.findAll());
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Object> getTeam(@PathVariable String name) {
+        var team = teamRepository.findByName(name);
+        if (team.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Team not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(team);
+    }
 }
--- src/test/java/org/example/pokemonmasterapi/TeamTest.java
+++ src/test/java/org/example/pokemonmasterapi/TeamTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -69,5 +70,51 @@ class TeamTest {
         response.andExpect(status().isConflict());
         response.andExpect(content().string("Team already exists"));
 
+    }
+
+    @Test
+    public void returnAllTeamsOkStatus() throws Exception {
+        // Given
+        mockMvc.perform(post("/teams")
+                .content("{\"name\": \"Team Rocket\"}")
+                .contentType(MediaType.APPLICATION_JSON));
+        mockMvc.perform(post("/teams")
+                .content("{\"name\": \"Team Aqua\"}")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // When
+        var response = mockMvc.perform(get("/teams"));
+
+        // Then
+        response.andExpect(status().isOk());
+        assertThat(teamRepository.findAll()).hasSize(2);
+        assertThat(response.andReturn().getResponse().getContentAsString()).contains("Team Rocket", "Team Aqua");
+    }
+
+    @Test
+    public void returnOneTeamOkStatus() throws  Exception {
+        // Given
+        mockMvc.perform(post("/teams")
+                .content("{\"name\": \"Team Rocket\"}")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // When
+        var response = mockMvc.perform(get("/teams/Team Rocket"));
+
+        // Then
+        response.andExpect(status().isOk());
+        assertThat(response.andReturn().getResponse().getContentAsString()).contains("Team Rocket");
+    }
+
+    @Test
+    public void returnOneTeamNotFoundStatus() throws  Exception {
+        // Given
+
+        // When
+        var response = mockMvc.perform(get("/teams/Team Rocket"));
+
+        // Then
+        response.andExpect(status().isNotFound());
+        response.andExpect(content().string("Team not found"));
     }
 }
