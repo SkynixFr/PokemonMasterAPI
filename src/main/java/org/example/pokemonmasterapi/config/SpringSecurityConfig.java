7e95f94e31351f51006886ee6b7b61221adfad5b
Ricky Khao <103840102+Luffysonic@users.noreply.github.com>
2024-03-28 09:39:17 +0100

    feat(Security): ajout d'un Début Token JwT

--- src/main/java/org/example/pokemonmasterapi/config/SpringSecurityConfig.java
+++ src/main/java/org/example/pokemonmasterapi/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.example.pokemonmasterapi.config;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +11,20 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 
 @Configuration
 public class SpringSecurityConfig {
-
+    private String jwtKey = "dcf59fce3f5afac9ec0c2abccbb84c6c0b33f5893c2ca13cc651ac52bd7a8f0e";
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -26,6 +34,7 @@ public class SpringSecurityConfig {
 
                                 .requestMatchers("user/register").permitAll()
                                 .anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
@@ -40,6 +49,16 @@ public class SpringSecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length, "RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
     }
 }
 
--- src/main/java/org/example/pokemonmasterapi/controllers/UserController.java
+++ src/main/java/org/example/pokemonmasterapi/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.example.pokemonmasterapi.controllers;
 
 import lombok.AllArgsConstructor;
+import org.example.pokemonmasterapi.config.JWTService;
 import org.example.pokemonmasterapi.model.User;
 import org.example.pokemonmasterapi.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,9 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     private final UserRepository userRepository;
-
+    private final JWTService jwtService;
     @PostMapping("/register")
-public ResponseEntity<Object> register(@RequestBody User user) {
+    public ResponseEntity<Object> register(@RequestBody User user) {
         if (user.getUsername() == null || user.getPassword() == null || user.getEmail() == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing required data. Please provide email, username, and password");
         }
@@ -34,4 +36,11 @@ public ResponseEntity<Object> register(@RequestBody User user) {
         userRepository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("User created");
     }
+
+    @PostMapping("/login")
+    public String getToken(Authentication authentication) {
+        String token = jwtService.generateToken(authentication);
+        return token;
+    }
+
 }
