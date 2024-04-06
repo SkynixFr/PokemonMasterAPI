42efdbc10b8dcc13304555fc1b0ab38874ea2397
SkynixFr <romain.ludovic.day@gmail.com>
2024-04-06 16:35:11 +0200

    feat(avatar): update avatar model

--- src/main/java/org/example/pokemonmasterapi/model/Avatar.java
+++ src/main/java/org/example/pokemonmasterapi/model/Avatar.java
@@ -3,7 +3,9 @@ package org.example.pokemonmasterapi.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "avatars")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
