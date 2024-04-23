abfefe6239458f926f471fcdf76569a361c97ae2
SkynixFr <romain.ludovic.day@gmail.com>
2024-04-23 21:51:57 +0200

    feat(avatar): delete avatar update route

--- src/test/java/org/example/pokemonmasterapi/AvatarTest.java
+++ src/test/java/org/example/pokemonmasterapi/AvatarTest.java
@@ -93,61 +93,6 @@ class AvatarTest {
     }
 
     @Test
-    public void updateAvatarReturnOkStatus() throws Exception {
-        //Given
-        var avatarId = avatarRepository.save(
-                new AvatarEntity(null, "Red", "Kanto", "~/public/images/compressed/avatars/kanto/Ash.png")).getId();
-
-        //When
-        var response = mockMvc.perform(put("/avatars/" + avatarId)
-                .content(
-                        "{\"name\": \"Blue\",\"region\": \"Kanto\",\"sprite\": \"~/public/images/compressed/avatars/kanto/Blue.png\"}")
-                .contentType(MediaType.APPLICATION_JSON));
-
-        //Then
-        response.andExpect(status().isOk());
-        response.andExpect(content().json(
-                "{\"name\":\"Blue\",\"region\":\"Kanto\",\"sprite\":\"~/public/images/compressed/avatars/kanto/Blue.png\"}"));
-    }
-
-    @Test
-    public void updateAvatarReturnConflictStatus() throws Exception {
-        //Given
-        avatarRepository.save(
-                new AvatarEntity(null, "Red", "Kanto", "~/public/images/compressed/avatars/kanto/Ash.png"));
-        var avatarId = avatarRepository.save(
-                new AvatarEntity(null, "Blue", "Kanto", "~/public/images/compressed/avatars/kanto/Blue.png")).getId();
-
-        //When
-        var response = mockMvc.perform(put("/avatars/" + avatarId)
-                .content(
-                        "{\"name\": \"Red\",\"region\": \"Kanto\",\"sprite\": \"~/public/images/compressed/avatars/kanto/Ash.png\"}")
-                .contentType(MediaType.APPLICATION_JSON));
-
-        //Then
-        response.andExpect(status().isConflict());
-        response.andExpect(status().reason("Avatar with name Red already exists"));
-    }
-
-    @Test
-    public void updateAvatarReturnNotFoundStatus() throws Exception {
-        //Given
-        var avatarId = avatarRepository.save(
-                new AvatarEntity(null, "Red", "Kanto", "~/public/images/compressed/avatars/kanto/Ash.png")).getId();
-        avatarRepository.deleteAll();
-
-        //When
-        var response = mockMvc.perform(put("/avatars/" + avatarId)
-                .content(
-                        "{\"name\": \"Blue\",\"region\": \"Kanto\",\"sprite\": \"~/public/images/compressed/avatars/kanto/Blue.png\"}")
-                .contentType(MediaType.APPLICATION_JSON));
-
-        //Then
-        response.andExpect(status().isNotFound());
-        response.andExpect(status().reason("Avatar with id " + avatarId + " not found"));
-    }
-
-    @Test
     public void deleteAvatarReturnNoContentStatus() throws Exception {
         //Given
         var avatarId = avatarRepository.save(
